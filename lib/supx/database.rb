6cea6d8afb335899cfed1da2e6cacd17724aa558
jibi <jibi@paranoici.org>
2014-06-05 17:25:34 +0200

    Code cleanup

--- lib/supx/database.rb
+++ lib/supx/database.rb
@@ -10,80 +10,80 @@
 
 module Supx
 module Database
-	@messages_schema = Proc.new {
-		primary_key :_id
-		String      :key_remote_jid
-		Integer     :key_from_me
-		String      :key_id
-		Integer     :status
-		Integer     :needs_push
-		String      :data
-		Integer     :timestamp
-		String      :media_url
-		String      :media_mime_type
-		String      :media_wa_type
-		Integer     :media_size
-		String      :media_name
-		Float       :latitude
-		Float       :longitude
-		String      :thumb_image
-		String      :remote_resource
-		Integer     :received_timestamp
-		Integer     :send_timestamp
-		Integer     :receipt_server_timestamp
-		Integer     :receipt_device_timestamp
-		Blob        :raw_data
-		String      :media_hash
-		Int         :recipient_count
-		Integer     :media_duration
-		Integer     :origin
-	}
+  @messages_schema = Proc.new {
+    primary_key :_id
+    String      :key_remote_jid
+    Integer     :key_from_me
+    String      :key_id
+    Integer     :status
+    Integer     :needs_push
+    String      :data
+    Integer     :timestamp
+    String      :media_url
+    String      :media_mime_type
+    String      :media_wa_type
+    Integer     :media_size
+    String      :media_name
+    Float       :latitude
+    Float       :longitude
+    String      :thumb_image
+    String      :remote_resource
+    Integer     :received_timestamp
+    Integer     :send_timestamp
+    Integer     :receipt_server_timestamp
+    Integer     :receipt_device_timestamp
+    Blob        :raw_data
+    String      :media_hash
+    Int         :recipient_count
+    Integer     :media_duration
+    Integer     :origin
+  }
 
-	@chat_list_schema = Proc.new {
-		primary_key :_id
-		String      :key_remote_jid
-		Integer     :message_table_id
-	}
+  @chat_list_schema = Proc.new {
+    primary_key :_id
+    String      :key_remote_jid
+    Integer     :message_table_id
+  }
 
-	@contacts_schema = Proc.new {
-		primary_key :id
-		String      :jid
-		Bool        :is_whatsapp_user
-		String      :status
-		String      :number
-		Integer     :raw_contact_id
-		String      :display_name
-		Integer     :phone_type
-		String      :phone_label
-		Integer     :unseen_msg_count
-		Integer     :photo_ts
-		Integer     :thumb_ts
-		Integer     :photo_id_timestamp
-		String      :given_name
-		String      :family_name
-		String      :wa_name
-		String      :sort_name
-	}
-	class << self
-		attr_reader :messages_schema, :chat_list_schema
-		attr_reader :contacts_schema
-	end
+  @contacts_schema = Proc.new {
+    primary_key :id
+    String      :jid
+    Bool        :is_whatsapp_user
+    String      :status
+    String      :number
+    Integer     :raw_contact_id
+    String      :display_name
+    Integer     :phone_type
+    String      :phone_label
+    Integer     :unseen_msg_count
+    Integer     :photo_ts
+    Integer     :thumb_ts
+    Integer     :photo_id_timestamp
+    String      :given_name
+    String      :family_name
+    String      :wa_name
+    String      :sort_name
+  }
+  class << self
+    attr_reader :messages_schema, :chat_list_schema
+    attr_reader :contacts_schema
+  end
 
-	def db_connect
-		raise 'No messages database found' if not File.exists?(@msg_db_path)
+  def db_connect
+    raise 'No messages database found' if not File.exists?(@msg_db_path)
 
-		@message_db = Sequel.connect('sqlite://' + @msg_db_path)
-		@message_db.create_table?(:messages,  &Supx::Database.messages_schema)
-		@message_db.create_table?(:chat_list, &Supx::Database.chat_list_schema)
+    @message_db = Sequel.connect('sqlite://' + @msg_db_path)
+    @message_db.create_table?(:messages,  &Supx::Database.messages_schema)
+    @message_db.create_table?(:chat_list, &Supx::Database.chat_list_schema)
 
-		if File.exists?(@contact_db_path)
-			@contacts   = true
-			@contact_db = Sequel.connect('sqlite://' + @contact_db_path)
-			@contact_db.create_table?(:wa_contacts, &Supx::Database.contacts_schema)
-		else
-			@contact = false
-		end
+    if File.exists?(@contact_db_path)
+      @contacts   = true
+      @contact_db = Sequel.connect('sqlite://' + @contact_db_path)
+      @contact_db.create_table?(:wa_contacts, &Supx::Database.contacts_schema)
+    else
+      @contact = false
+    end
 
-	end
+  end
 end
 end
--- lib/supx/supx.rb
+++ lib/supx/supx.rb
@@ -12,169 +12,175 @@ require 'sequel'
 module Supx
 
 class SupxManager
-	include Database
-	include Smile
+  include Database
+  include Smile
 
-	def initialize(path)
-		@data_folder     = path
-		@msg_db_path     = path + '/msgstore.db'
-		@contact_db_path = path + '/wa.db'
+  def initialize(path)
+    @data_folder     = path
+    @msg_db_path     = path + '/msgstore.db'
+    @contact_db_path = path + '/wa.db'
 
-		db_connect
-	end
+    db_connect
+  end
 
-	def read_messages(contact)
-		if contact =~ /@s.whatsapp.net/
-			@conversation =:single
-		elsif contact =~ /@g.us/
-			@conversation = :group
-		else
-			raise 'Invalid contact'
-		end
+  def set_conversation_type
+    if @contact =~ /@s.whatsapp.net/
+      @conversation =:single
+    elsif @contact =~ /@g.us/
+      @conversation = :group
+    else
+      raise 'Invalid contact'
+    end
+  end
 
-		open_html
+  def parse_msg
+      return if @conversation == :group and @cur_msg[:media_wa_type].eql?(0) and @cur_msg[:media_size].nonzero?
 
-		@message_db[:messages].filter(:key_remote_jid => contact).order(:_id).each do |msg|
+      $stderr.print "\r" + @cur_msg[:_id].to_s
 
-			# XXX: dont know what kind of message is this (but we
-			# dont want to show these messages)
-			next if @conversation == :group and msg[:media_wa_type].eql?(0) and msg[:media_size].nonzero?
+      html_date_changed
+      html_msg
+  end
 
-			@msg = msg
-			$stderr.print "\r" + msg[:_id].to_s
+  def read_messages(contact)
+    @contact = contact
 
-			html_date
-			html_msg_content
-		end
+    set_conversation_type
+    open_html
 
-		close_html
-	end
+    @message_db[:messages].filter(:key_remote_jid => contact).order(:_id).each do |msg|
+      @cur_msg = msg
+      parse_msg
+    end
 
-	def contact_list
-		slist = []
-		glist = []
+    html_close
+  end
 
-		@message_db[:chat_list].where{ Sequel.like(:key_remote_jid, '%@s%%') | Sequel.like(:key_remote_jid, '%@g%%') }.each do |m|
-			next if m.nil?
+  def contact_list
+    slist = []
+    glist = []
 
-			c = { :id => m[:key_remote_jid] }
+    @message_db[:chat_list].where{ Sequel.like(:key_remote_jid, '%@s%%') | Sequel.like(:key_remote_jid, '%@g%%') }.each do |m|
+      next if m.nil?
 
-			if @contacts == true
-				contact = @contact_db[:wa_contacts].where(:jid => m[:key_remote_jid])
-				next if contact.count.zero?
+      c = { :id => m[:key_remote_jid] }
 
-				c[:name] = contact.first[:display_name]
-			end
+      if @contacts == true
+        contact = @contact_db[:wa_contacts].where(:jid => m[:key_remote_jid])
+        next if contact.count.zero?
 
-			if m[:key_remote_jid] =~ /@s/
-				slist << c
-			else
-				glist << c
-			end
-		end
+        c[:name] = contact.first[:display_name]
+      end
 
-		{:contacts => slist, :groups => glist}
-	end
+      if m[:key_remote_jid] =~ /@s/
+        slist << c
+      else
+        glist << c
+      end
+    end
+
+    {:contacts => slist, :groups => glist}
+  end
 
 private
 
-	def open_html
-		@html = <<END
-<html><head>
+  def open_html
+    @html = "<html><head>
 <style>
-	body { background-color: #f1eae0; font: 'lucida grande, thaoma, verdana'; }
-	#container {width: 800px; margin: 0px auto }
-	.msg { width: 80%; background: #dde; padding: 16px; overflow: hidden; border-radius: 4px; }
-	.msg > .g_sender { font-weight: bold; padding-bottom: 12px }
-	.msg > .time { margin-top: 16px; font-size: 9px; }
-	.r { background-color: #f3ffe9; float: right; text-align: right; }
-	.l { background-color: #fcfbf6; float: left; }
-	.date { background-color: #d8eff7; width: 200px; margin: 16px auto; border-radius: 4px; border: 4px; height: 24;  text-align: center}
-	#wrapper { overflow:auto; padding: 4px; }
+  body { background-color: #f1eae0; font: 'lucida grande, thaoma, verdana'; }
+  #container {width: 800px; margin: 0px auto }
+  .msg { width: 80%; background: #dde; padding: 16px; overflow: hidden; border-radius: 4px; }
+  .msg > .g_sender { font-weight: bold; padding-bottom: 12px }
+  .msg > .time { margin-top: 16px; font-size: 9px; }
+  .r { background-color: #f3ffe9; float: right; text-align: right; }
+  .l { background-color: #fcfbf6; float: left; }
+  .date { background-color: #d8eff7; width: 200px; margin: 16px auto; border-radius: 4px; border: 4px; height: 24;  text-align: center}
+  #wrapper { overflow:auto; padding: 4px; }
 </style>
-</head><body><div id=container>
-END
-	end
+</head><body><div id=container>"
+  end
 
-	def close_html
-		@html += "</div></body></html>"
-	end
+  def html_close
+    @html += "</div></body></html>"
+  end
 
-	def html_date
-		timestamp = @msg[:timestamp]
-		date      = Time.at(timestamp.to_s[0..-4].to_i).strftime('%Y-%m-%d')
+  def html_date_changed
+    timestamp = @cur_msg[:timestamp]
+    date      = Time.at(timestamp.to_s[0..-4].to_i).strftime('%Y-%m-%d')
 
-		if @last_date.nil? or @last_date != date
-			@last_date =  date
-			@html      += "<div class='date'>" + date + "</div>"
-		end
-	end
+    if @last_date.nil? or @last_date != date
+      @last_date =  date
+      @html      += "<div class='date'>" + date + "</div>"
+    end
+  end
 
-	def html_msg_content
-		@html += "<div id='wrapper'><div class='msg " + (@msg[:key_from_me] == 1 ? "r" : "l") + "'>"
+  def get_contact_name(contact)
+      if @contacts
+        return @contact_db[:wa_contacts].where(:jid => contact).first[:display_name]
+      else
+        return @cur_msg[:remote_resource]
+      end
+  end
 
-		if @conversation == :group and @msg[:key_from_me] == 0
-			@html += "<div class='g_sender'>"
-			if @contacts
-				@html += @contact_db[:wa_contacts].where(:jid => @msg[:remote_resource]).first[:display_name]
-			else
-				@html += @msg[:remote_resource]
-			end
-			@html += "</div>"
-		end
+  def html_msg
+    @html += "<div id='wrapper'><div class='msg " + (@cur_msg[:key_from_me] == 1 ? "r" : "l") + "'>"
 
-		@html += case @msg[:media_wa_type]
-		when "0"
-			get_msg_text
-		when "1"
-			get_msg_media
-		else
-			"[TODO: add other medias]"
-		end
+    if @conversation == :group and @cur_msg[:key_from_me] == 0
+      @html += "<div class='g_sender'>" + get_contact_name(@cur_msg[:remote_resource]) + "</div>"
+    end
 
-		time  =  Time.at(@msg[:timestamp].to_s[0..-4].to_i).strftime("%l:%M%p")
-		@html += "<div class=time>" + time + "</div>"
+    @html += case @cur_msg[:media_wa_type]
+    when "0"
+      get_msg_text
+    when "1"
+      get_msg_media
+    else
+      "[TODO: add other medias]"
+    end
 
-		@html += "</div></div>\n"
-	end
+    time  =  Time.at(@cur_msg[:timestamp].to_s[0..-4].to_i).strftime("%l:%M%p")
+    @html += "<div class=time>" + time + "</div>"
 
-	def get_msg_text
-		data = @msg[:data]
+    @html += "</div></div>\n"
+  end
 
-		if data.nil?
-			data = ""
-		else
-			$smiles.each do |s|
-				data = data.gsub(s[0], "<img src='imgs/" + s[1] + "'>")
-			end
-		end
+  def get_msg_text
+    data = @cur_msg[:data]
 
-		data
-	end
+    if data.nil?
+      data = ""
+    else
+      $smiles.each do |s|
+        data = data.gsub(s[0], "<img src='imgs/" + s[1] + "'>")
+      end
+    end
 
-	def get_msg_media
-		if @msg[:key_from_me] == 0
-			media_size = @msg[:media_size]
-			timestamp  = @msg[:timestamp]
+    data
+  end
 
-			media_name = find_photo(timestamp, media_size)
-			data = "<img src='#{@data_folder}/Media/WhatsApp Images/#{media_name}'>"
-		else
-			media_name = @msg[:media_name]
-			data = "<img src='#{@data_folder}/Media/WhatsApp Images/Sent/#{media_name}'>"
-		end
+  def get_msg_media
+    if @cur_msg[:key_from_me] == 0
+      media_size = @cur_msg[:media_size]
+      timestamp  = @cur_msg[:timestamp]
 
-		data
-	end
+      media_name = find_photo(timestamp, media_size)
+      data = "<img src='#{@data_folder}/Media/WhatsApp Images/#{media_name}'>"
+    else
+      media_name = @cur_msg[:media_name]
+      data = "<img src='#{@data_folder}/Media/WhatsApp Images/Sent/#{media_name}'>"
+    end
 
-	def find_photo(timestamp, media_size)
-		date = Time.at(timestamp.to_s[0..-4].to_i).strftime("%Y%m%d")
+    data
+  end
 
-		Dir.entries("./#{@data_folder}/Media/WhatsApp Images/").grep(Regexp.new("IMG-" + date)).each do |p|
-			path = "./data/Media/WhatsApp Images/" + p
-			return p if File.size(path) == media_size
-		end
-	end
+  def find_photo(timestamp, media_size)
+    date = Time.at(timestamp.to_s[0..-4].to_i).strftime("%Y%m%d")
+
+    Dir.entries("./#{@data_folder}/Media/WhatsApp Images/").grep(Regexp.new("IMG-" + date)).each do |p|
+      path = "./data/Media/WhatsApp Images/" + p
+      return p if File.size(path) == media_size
+    end
+  end
 end
 end
 
